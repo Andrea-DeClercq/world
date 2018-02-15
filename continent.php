ff354e211674fac7bb14d132791875f1e67a6f27
Pikou <andrea.dclercq@gmail.com>
2018-02-15 11:02:59 +0100

    Mise en page pour le tableau continent

---
+++ continent.php
@@ -0,0 +1,40 @@
+<?php require_once 'header.php';
+session_start ();?>
+// On teste pour voir si nos variables de session ont bien été enregistrées
+<?php
+require_once 'inc/manager-db.php';
+$continent = $_GET['continent'];
+$pays = getCountriesByContinent($continent);
+//var_dump($pays[0]);
+if (isset($_SESSION['nom']) && isset($_SESSION['role'])) {
+    echo "<p style=text-align:left;>Bienvenue : " . $_SESSION['nom'] . "(" . $_SESSION['role'] . ")";
+    echo '<br><a href="./logout.php">Deconnexion</a></p>';
+}
+?>
+<h1><?php echo $continent ?></h1>
+<table class="ui celled table">
+    <thead>
+    <tr>
+        <th>Nom</th>
+        <th>Continent</th>
+        <th>Region</th>
+        <th>Surface du pays</th>
+        <th>Année d'indépendance</th>
+        <th>Population</th>
+        <?php
+        foreach($pays as $p ){
+        $href = "<a href='pays.php?idCountry=" . $p->id . "&amp;continent=" . $p->Continent . "'>";
+        ?>
+    <tr>
+        <td><?php echo $href.$p->Name ?></a></td>
+        <td><?php echo $p->Continent ?></td>
+        <td><?php echo $p->Region ?></td>
+        <td><?php echo $p->SurfaceArea ?></td>
+        <td><?php echo $p->IndepYear ?></td>
+        <td><?php echo $p->Population ?></td>
+    </tr>
+    <?php }?>
+</table>
+<?php
+require_once 'javascripts.php';
+require_once 'footer.php'; ?>
--- index.php
+++ index.php
@@ -1,58 +1,7 @@
 <?php require_once 'header.php';
 session_start ();?>
 // On teste pour voir si nos variables de session ont bien été enregistrées
-
-<div class="ui container main">
-    <div class="ui dropdown">
-        <div class="text">Continent</div>
-        <i class="dropdown icon"></i>
-        <div class="menu">
-            <div class="item"><a href="?continent=North%20America">North America</a></div>
-            <div class="item"><a href="?continent=South%20America">South America</a></div>
-            <div class="item"><a href="?continent=Africa">Africa</a></div>
-            <div class="item"><a href="?continent=Europe">Europe</a></div>
-            <div class="item"><a href="?continent=Asia">Asia</a></div>
-            <div class="item"><a href="?continent=Oceania">Oceania</a></div>
-        </div>
-    </div>
-    <div>
-      <?php
-      require_once 'inc/manager-db.php';
-      $continent = $_GET['continent'];
-      $pays = getCountriesByContinent($continent);
-      //var_dump($pays[0]);
-      if (isset($_SESSION['nom']) && isset($_SESSION['role'])) {
-          echo "<p style=text-align:left;>Bienvenue : " . $_SESSION['nom'] . "(" . $_SESSION['role'] . ")";
-          echo '<br><a href="./logout.php">Deconnexion</a></p>';
-      }
-        ?>
-    </div>
-    <h1><?php echo $continent ?></h1>
-    <table class="ui celled table">
-        <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Continent</th>
-            <th>Region</th>
-            <th>Surface du pays</th>
-            <th>Année d'indépendance</th>
-            <th>Population</th>
-                <?php
-                foreach($pays as $p ){
-                    $href = "<a href='pays.php?idCountry=" . $p->id . "&amp;continent=" . $p->Continent . "'>";
-                    ?>
-                    <tr>
-                    <td><?php echo $href.$p->Name ?></a></td>
-                    <td><?php echo $p->Continent ?></td>
-                    <td><?php echo $p->Region ?></td>
-                    <td><?php echo $p->SurfaceArea ?></td>
-                    <td><?php echo $p->IndepYear ?></td>
-                    <td><?php echo $p->Population ?></td>
-                    </tr>
-                    <?php }?>
-        </tr>
-    </table>
-    <p></p>
+    <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH</p>
 
     <div class="ui one column grid">
         <div class="column">
@@ -75,7 +24,6 @@ session_start ();?>
             </div>
         </div>
     </div>
-</div>
 
 <?php
 require_once 'javascripts.php';
