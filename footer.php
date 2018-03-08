727e47441861e90e12a20c58ae98205911b49791
TwioZz <yoann.lebreton91@gmail.com>
2018-03-08 08:58:59 +0100

    Ester Egss

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <hr />
     <footer style="padding-bottom: 2em">
-      <div style="text-align: center">MyWebApp &copy; 2018</div>
+      <div style="text-align: center">MyIssouApp &copy; 18-25</div>
      </footer>
 
   </body>
